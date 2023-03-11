42349b3ff00f2e4508576b0c93f22cfd8cb8c29d
sulmi24 <a.sulmi24@gmail.com>
2023-03-11 19:57:18 +0200

    array js  is done

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  return `${cats.push(name)}`;
+}
+
+destructivelyAppendCat("Ralph");
+
+function destructivelyPrependCat(name) {
+  return `${cats.unshift(name)}`;
+}
+
+destructivelyPrependCat("Bob");
+
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+
+function destructivelyRemoveLastCat() {
+  return `${cats.pop()}`;
+}
+destructivelyRemoveLastCat();
+
+cats.length = 0;
+
+cats.push("Milo", "Otis", "Garfield");
+
+function destructivelyRemoveFirstCat() {
+  return `${cats.shift()}`;
+}
+destructivelyRemoveFirstCat();
+
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+
+function appendCat(name) {
+  let newcats = [...cats];
+  newcats.push(name);
+  return newcats;
+}
+
+console.log(appendCat("Broom"));
+
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+
+function prependCat(name) {
+  let newcats = [...cats];
+  newcats.unshift(name);
+  return newcats;
+}
+console.log(appendCat("Arnolds"));
+
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+
+function removeLastCat() {
+  let newcats = [...cats];
+  newcats.pop();
+  return newcats;
+}
+
+console.log(appendCat());
+
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+
+function removeFirstCat() {
+  let newcats = [...cats];
+  newcats.shift();
+  return newcats;
+}
+console.log(appendCat());
